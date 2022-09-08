050904d875ccd6d082ad9e332a6d27b7e4398cd4
Florian Jahn <florian@involve.at>
2022-09-08 12:28:47 +0200

    Set tracking cookie to SameSite=Strict
    This prevents a warning message on firefox

--- src/examples/tracker.js
+++ src/examples/tracker.js
@@ -1,13 +1,13 @@
-console.log('This is an example of an external tracking script.');
+console.log('This is an example of an external tracking script');
 
-function setCookie(name, value, days) {
-    var expires = '';
+function setCookie (name, value, days) {
+    let expires = '';
     if (days) {
-        var date = new Date();
+        const date = new Date();
         date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
         expires = '; expires=' + date.toUTCString();
     }
-    document.cookie = name + '=' + (value || '') + expires + '; path=/';
+    document.cookie = name + '=' + (value || '') + expires + '; path=/; SameSite=Strict';
 }
 
 //we set a tracking cookie as an example
