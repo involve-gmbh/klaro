ae281cc5bcd88a3dda51f9c8b3a91ffc1bec0d40
Florian Jahn <florian@involve.at>
2022-09-09 10:30:14 +0200

    Import translations using glob import

--- src/translations/README.md
+++ src/translations/README.md
@@ -13,6 +13,6 @@ ensure that the string will be capitalized in all Klaro languages.
 
 We try to keep translation files in sync by automatically removing obsolete translations and
 marking missing translation strings. A missing translation will be indicated by an empty string ''
-in the given key. An key with name `[original_key]_en` will be added, containing the English
+in the given key. A key with name `[original_key]_en` will be added, containing the English
 translation for the missing key (so you won't have to look it up by hand to know what should go there).
 You can simply add the missing translation and remove the `_en` key.
--- src/translations/index.js
+++ src/translations/index.js
@@ -1,54 +1,17 @@
-// To create a new translation, add a YAML file with the required entries and
-// link it here using the appropriate language code.
+const translationFileNamePattern = /\.\/(\w+)\.yml/;
 
-import zh from './zh.yml';
-import pt from './pt.yml';
-import ca from './ca.yml';
-import cs from './cs.yml';
-import da from './da.yml';
-import de from './de.yml';
-import el from './el.yml';
-import en from './en.yml';
-import es from './es.yml';
-import fi from './fi.yml';
-import fr from './fr.yml';
-import gl from './gl.yml';
-import hu from './hu.yml';
-import hr from './hr.yml';
-import it from './it.yml';
-import nl from './nl.yml';
-import no from './no.yml';
-import ro from './ro.yml';
-import sr from './sr.yml';
-import sr_cyrl from './sr_cyrl.yml';
-import sv from './sv.yml';
-import tr from './tr.yml';
-import pl from './pl.yml';
-import ru from './ru.yml';
+const yamlFiles = import.meta.glob('./*.yml', { eager: true });
 
-export default {
-    ca,
-    cs,
-    da,
-    de,
-    el,
-    en,
-    zh,
-    pt,
-    es,
-    fi,
-    fr,
-    gl,
-    hu,
-    hr,
-    it,
-    nl,
-    no,
-    ro,
-    sr,
-    sr_cyrl,
-    sv,
-    tr,
-    pl,
-    ru,
-};
+const fileNames = Object.keys(yamlFiles);
+
+const translationFileNames = fileNames.filter(fileName => translationFileNamePattern.test(fileName));
+
+const translations = translationFileNames.reduce((acc, fileName) => {
+    const languageName = translationFileNamePattern.exec(fileName)[1];
+    const languageFile = yamlFiles[fileName];
+
+    acc[languageName] = languageFile;
+    return acc;
+}, {});
+
+export default translations;
